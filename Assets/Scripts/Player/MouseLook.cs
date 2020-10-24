11bd72e7f31352a1cddbba030b5384ebb68f2109
Michael Hayter <contact@michaelhayterphotography.com>
2020-10-24 10:03:30 +0800

    First sandpit rooms created. Player created and Movement.cs and MouseLook.cs created to allow player movement

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,40 @@
+﻿using CreateWithCodeGameJam2020.Utility;
+using UnityEngine;
+
+namespace CreateWithCodeGameJam2020.Player
+{
+    public class MouseLook : MonoSingleton<MouseLook>
+    {
+        [SerializeField]
+        private float _mouseSensativity = 50f;
+
+        [SerializeField]
+        private Transform _player;
+
+        private float _xRotation = 0f;
+
+        private void Start()
+        {
+            //make sure cursor is not visible in game
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Get mouse input
+            float mouseX = Input.GetAxis("Mouse X") * _mouseSensativity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * _mouseSensativity * Time.deltaTime;
+
+            
+            //Convert Y direction to face up and down
+            _xRotation -= mouseY;
+            //clamp up and down
+            _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+            //Convert X direction to rotate player
+            _player.Rotate(Vector3.up, mouseX);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController _characterController;
+
+    [SerializeField]
+    private float _movementSpeed;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //get player input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+
+
+        //move player based on input
+        _characterController.Move(move * _movementSpeed * Time.deltaTime);
+    }
+}
